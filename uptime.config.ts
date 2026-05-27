47e8d60b12c36da251f1bd21637cc206ecef34cf
game_ryo <ryotagtagtag@gmail.com>
2026-05-27 16:35:40 +0900

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -60,67 +60,39 @@ const workerConfig: WorkerConfig = {
       // checkProxyFallback: true,
     
 
-  // [Optional] Notification settings
+   // [Optional] Notification settings
   notification: {
-    // [Optional] Notification webhook settings, if not specified, no notification will be sent
-    // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
     webhook: {
-      // [Required] webhook URL (example: Telegram Bot API)
-      url: 'https://api.telegram.org/bot123456:ABCDEF/sendMessage',
-      // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
-      // method: 'POST',
-      // [Optional] headers to be sent
-      // headers: {
-      //   foo: 'bar',
-      // },
-      // [Required] Specify how to encode the payload
-      // Should be one of 'param', 'json' or 'x-www-form-urlencoded'
-      // 'param': append url-encoded payload to URL search parameters
-      // 'json': POST json payload as body, set content-type header to 'application/json'
-      // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
-      payloadType: 'x-www-form-urlencoded',
-      // [Required] payload to be sent
-      // $MSG will be replaced with the human-readable notification message
-      payload: {
-        chat_id: 12345678,
-        text: '$MSG',
+      // 自分の「ユーザー名」と「リポジトリ名」に書き換えてください
+      url: 'https://github.com/ryopc/ryopc-Status/dispatches',
+      method: 'POST',
+      headers: {
+        // ステップ1で取得したトークンを、ghp_...のままここに貼り付けます
+        Authorization: `token ${env.GH_NOTIFY_TOKEN}`,
+        Accept: 'application/vnd.github.v3+json',
+        'User-Agent': 'UptimeFlare-Monitor',
       },
-      // [Optional] timeout calling this webhook, in millisecond, default to 5000
+      payloadType: 'json',
+      payload: {
+        event_type: 'trigger_issue',
+        client_payload: {
+          text: '$MSG', // ここに障害メッセージが入ります
+        },
+      },
       timeout: 10000,
     },
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
-    timeZone: 'Asia/Shanghai',
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
-    gracePeriod: 5,
+    timeZone: 'Asia/Tokyo',
+    gracePeriod: 0, // 落ちたらすぐにIssueを作りたいので0にします
   },
 }
+
 
 // You can define multiple maintenances here
 // During maintenance, an alert will be shown at status page
 // Also, related downtime notifications will be skipped (if any)
 // Of course, you can leave it empty if you don't need this feature
 
-// const maintenances: MaintenanceConfig[] = []
 
-const maintenances: MaintenanceConfig[] = [
-  {
-    // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['foo_monitor', 'bar_monitor'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
-    title: 'Test Maintenance',
-    // Description of the maintenance, will be shown at status page
-    body: 'This is a test maintenance, server software upgrade',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2020-01-01T00:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
-    end: '2050-01-01T00:00:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
-    color: 'blue',
-  },
-]
 
 // Don't edit this line
 export { maintenances, pageConfig, workerConfig }
